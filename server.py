cd357d2db050e2af467823a0b95a3422757d314c
rathodneha90 <ehanashrma87@gmail.com>
2019-08-17 08:34:33 +0530

    Created contacts route.

--- server.py
+++ server.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+import os
 
 
 @app.route('/')
 def home():
-    return "Hello world!"
+    return render_template("home.html")
+
+
+@app.route('/contacts')
+def contacts():
+    return render_template("contacts.html",
+                           message="following are my contacts",
+                           contacts=[i for i in range(10)])
 
 
 if __name__ == '__main__':
